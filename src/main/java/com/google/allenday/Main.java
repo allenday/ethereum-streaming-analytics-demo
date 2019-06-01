a2e797757ec4d1f3d58b9359bfe8a9d673279480
oleksandr.bushkovskyi <bushkovsky@gmail.com>
2019-06-02 01:31:35 +0300

    Use sliding windows

--- src/main/java/com/google/allenday/Main.java
+++ src/main/java/com/google/allenday/Main.java
@@ -11,25 +11,21 @@ import org.apache.beam.sdk.io.gcp.pubsub.PubsubIO;
 import org.apache.beam.sdk.io.gcp.pubsub.PubsubMessage;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
-import org.apache.beam.sdk.transforms.DoFn;
-import org.apache.beam.sdk.transforms.GroupByKey;
-import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.*;
 import org.apache.beam.sdk.transforms.windowing.AfterProcessingTime;
-import org.apache.beam.sdk.transforms.windowing.FixedWindows;
+import org.apache.beam.sdk.transforms.windowing.SlidingWindows;
 import org.apache.beam.sdk.transforms.windowing.Window;
-import org.apache.beam.sdk.values.KV;
 import org.joda.time.Duration;
 
 import java.io.IOException;
-import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) {
-        PipelineOptions options = PipelineOptionsFactory.create();
+        PipelineOptions options = PipelineOptionsFactory.fromArgs(args).create();
         Pipeline pipeline = Pipeline.create(options);
         pipeline.apply("Reading PubSub", PubsubIO
                 .readMessagesWithAttributes()
-                .fromSubscription("projects/crypto-etl-ethereum-prod/subscriptions/allenday-streaming-test")
+                .fromTopic("projects/ethereum-streaming-dev/topics/crypto_ethereum.transactions")
                         )
                 .apply("Deserialize JSON",
                         ParDo.of(new DoFn<PubsubMessage, Transaction>() {
@@ -53,38 +49,48 @@ public class Main {
                             }
                         }))
                 .apply("Get Block ID and Gas",
-                        ParDo.of(new DoFn<Transaction, KV<Long, Long>>() {
+                        ParDo.of(new DoFn<Transaction, Long>() {
                             @ProcessElement
                             public void processElement(ProcessContext c) {
                                 Transaction tx = c.element();
-                                c.output(KV.of(tx.getBlockNumber(),tx.getReceiptCumulativeGasUsed()));
+                                c.output(tx.getReceiptCumulativeGasUsed());
                             }
                         }))
-                .apply(Window.<KV<Long, Long>>into(FixedWindows.of(Duration.standardSeconds(30)))
+                .apply(Window.<Long>into(
+                        SlidingWindows.of(Duration.standardSeconds(120))
+                                .every(Duration.standardSeconds(120)))
                         .triggering(
                                 AfterProcessingTime.pastFirstElementInPane()
                                         .plusDelayOf(Duration.standardSeconds(30)))
                         .withAllowedLateness(Duration.standardSeconds(30))
                         .discardingFiredPanes())
-                .apply(GroupByKey.<Long, Long>create())
-                .apply("Calculate Mean Gas per Block",
-                        ParDo.of(new DoFn<KV<Long, Iterable<Long>>, KV<Long, Double>>() {
+                .apply(Mean.<Long>globally().withoutDefaults())
+                .apply(ParDo.of(new DoFn<Double, Double>() {
                             @ProcessElement
                             public void processElement(ProcessContext c) {
-                                KV<Long, Iterable<Long>> kv = c.element();
-                                Long blockNumber = kv.getKey();
-                                Iterator<Long> values = kv.getValue().iterator();
-                                Long sum = 0L;
-                                Integer n = 0;
-                                while (values.hasNext()) {
-                                    sum += values.next();
-                                    n++;
-                                }
-                                Double mean = (sum+0D) / n;
-                                System.err.println(blockNumber + " " + mean);
-                                c.output(KV.of(blockNumber, mean));
+                                System.err.println(c.element());
+                                c.output(c.element());
                             }
-                        }))
+                }))
+//                .apply(GroupByKey.<Long, Long>create())
+//                .apply("Calculate Mean Gas per Block",
+//                        ParDo.of(new DoFn<KV<Long, Iterable<Long>>, KV<Long, Double>>() {
+//                            @ProcessElement
+//                            public void processElement(ProcessContext c) {
+//                                KV<Long, Iterable<Long>> kv = c.element();
+//                                Long blockNumber = kv.getKey();
+//                                Iterator<Long> values = kv.getValue().iterator();
+//                                Long sum = 0L;
+//                                Integer n = 0;
+//                                while (values.hasNext()) {
+//                                    sum += values.next();
+//                                    n++;
+//                                }
+//                                Double mean = (sum+0D) / n;
+//                                System.err.println(blockNumber + " " + mean);
+//                                c.output(KV.of(blockNumber, mean));
+//                            }
+//                        }))
                         ;
         pipeline.run();
     }
